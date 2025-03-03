3d0a1496071c431459d2c3ae07d460e08ae32819
DevSamuelBarbosa <samuelbarbosa170319@gmail.com>
2025-03-03 10:27:32 -0300

    Testando fonte

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,14 +1,9 @@
 import type { Metadata } from "next";
-import { Geist, Geist_Mono } from "next/font/google";
+import { Raleway } from "next/font/google";
 import "../styles/globals.css";
 
-const geistSans = Geist({
-  variable: "--font-geist-sans",
-  subsets: ["latin"],
-});
-
-const geistMono = Geist_Mono({
-  variable: "--font-geist-mono",
+const ralewaySans = Raleway({
+  variable: "--font-raleway-sans",
   subsets: ["latin"],
 });
 
@@ -24,7 +19,7 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="pt-BR">
-      <body className={`${geistSans.variable} ${geistMono.variable} antialiased`}>
+      <body className={`${ralewaySans.variable} antialiased`}>
         {children}
       </body>
     </html>
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,20 +1,7 @@
-import Image from "next/image";
-import { getImagePath } from "@/utils/getImagePath";
-
 export default function Home() {
-	const ambiente = process.env.NEXT_PUBLIC_APP_ENVIRONMENT;
 	return (
 		<div className="flex flex-col items-center justify-center h-dvh gap-8">
-			<Image
-				className="dark:invert"
-				src={getImagePath("/next.svg")}
-				alt="Next.js logo"
-				width={180}
-				height={38}
-				priority
-			/>
-
-			<h1 className="text-white text-6xl font-extrabold text-center">{ambiente}</h1>
+			<h1 className="text-white text-6xl font-extrabold text-center">Hello World</h1>
 		</div>
 	);
 }
