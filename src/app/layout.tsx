82ac22c38e591fa50465cce0748e37177f241f5f
DevSamuelBarbosa <samuelbarbosa170319@gmail.com>
2025-03-04 21:40:25 -0300

    Fix: Utilizando variáveis das fontes

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -29,7 +29,7 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="pt-BR">
-      <body className={`antialiased`}>
+      <body className={`${ralewaySans.variable} ${cinzelSans.variable} antialiased`}>
         <Header/>
         {children}
       </body>
