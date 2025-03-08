4ec1b79f78d3f164bd61e312c40f7c2f8b771d8d
DevSamuelBarbosa <samuelbarbosa170319@gmail.com>
2025-03-08 07:32:10 -0300

    Fix: Bg Hero Section

--- src/components/HeroSection.tsx
+++ src/components/HeroSection.tsx
@@ -1,7 +1,18 @@
+import Image from "next/image";
+import { getImagePath } from "@/utils/getImagePath";
+
 export default function HeroSection() {
 	return (
-		<section className="w-full h-[60vh] md:h-[80vh] bg-[url('/bg_hero_section2.webp')] bg-cover bg-[center_left_35%] md:bg-center bg-no-repeat flex items-center justify-center">
-			<div className="container h-full mx-auto flex justify-end items-center">
+		<section className="relative w-full h-[60vh] md:h-[80vh] flex items-center justify-center overflow-hidden">
+			<Image 
+				src={getImagePath("/bg_hero_section2.webp")}
+				alt="Hero Section Background"
+				fill
+				className="absolute top-0 left-0 w-full h-full object-cover object-[center] z-[-1]"
+				priority
+			/>
+
+			<div className="container h-full mx-auto flex justify-end items-center relative z-10">
 				<div className="w-full px-8 lg:w-1/2">
 					<h1 className="font-['Cinzel'] text-white text-3xl font-semibold [text-shadow:1px_1px_1px_rgba(0,0,0,0.9)] mb-2 leading-snug md:font-medium md:text-2xl lg:text-3xl xl:text-4xl 2xl:text-5xl">
 						Advocacia Especializada em Direito da Família em Marau
