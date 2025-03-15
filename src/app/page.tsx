44b609a0e7af6a074bedfd90e06ba6c85a6d5f8e
DevSamuelBarbosa <samuelbarbosa170319@gmail.com>
2025-03-15 09:02:44 -0300

    Feat: Seção introdução

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,11 @@
 import HeroSection from '@/components/HeroSection';
+import SectionIntroducao from '@/components/SectionIntroducao';
 
 export default function Home() {
 	return (
 		<main>
             <HeroSection/>
+            <SectionIntroducao/>
 		</main>
 	);
 }
